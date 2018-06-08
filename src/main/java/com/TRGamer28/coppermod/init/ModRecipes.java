3d29a32187103824fa6a1abf8cf84c47d49b3642
Sadrien <sadriennightshade@gmail.com>
2018-06-08 01:29:24 -0700

    spelling fix

--- src/main/java/com/TRGamer28/coppermod/init/ModRecipes.java
+++ src/main/java/com/TRGamer28/coppermod/init/ModRecipes.java
@@ -3,7 +3,7 @@ package com.TRGamer28.coppermod.init;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
-public class ModRecipies {
+public class ModRecipes {
 
 	public static void init() {
 		GameRegistry.addSmelting(ModBlocks.COPPER_ORE, new ItemStack(ModItems.COPPER_INGOT, 1), 2.0f);
